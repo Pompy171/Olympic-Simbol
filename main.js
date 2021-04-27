1fba03a9f2acf8f515a8982691929457884b435d
Pompy171 <77317518+Pompy171@users.noreply.github.com>
2021-04-27 19:57:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ cntx = canvas.getContext("2d");
 colour = "blue";
 cntx.beginPath();
 cntx.strokeStyle = colour;
-cntx.lineWidth = 2;
-cntx.arc(200, 200, 40, 0, Math.PI * 2);
+cntx.lineWidth = 5;
+cntx.arc(300, 300, 40, 0, Math.PI * 2);
+cntx.stroke();
+
+
+colour = "yellow";
+cntx.beginPath();
+cntx.strokeStyle = colour;
+cntx.lineWidth = 5;
+cntx.arc(350, 350, 40, 0, Math.PI * 2);
+cntx.stroke();
+
+
+colour = "black";
+cntx.beginPath();
+cntx.strokeStyle = colour;
+cntx.lineWidth = 5;
+cntx.arc(400, 300, 40, 0, Math.PI * 2);
+cntx.stroke();
+
+
+colour = "green";
+cntx.beginPath();
+cntx.strokeStyle = colour;
+cntx.lineWidth = 5;
+cntx.arc(450, 350, 40, 0, Math.PI * 2);
+cntx.stroke();
+
+
+colour = "red";
+cntx.beginPath();
+cntx.strokeStyle = colour;
+cntx.lineWidth = 5;
+cntx.arc(500, 300, 40, 0, Math.PI * 2);
 cntx.stroke();
